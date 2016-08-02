0185a4be7eab39acfcea52fa189e5c56bb66d219
Allan Jardine <allan.jardine@sprymedia.co.uk>
2016-08-02 15:11:58 +0100

    Buttons 1.2.2 release

--- js/dataTables.buttons.js
+++ js/dataTables.buttons.js
@@ -1,4 +1,4 @@
-/*! Buttons for DataTables 1.2.2-dev
+/*! Buttons for DataTables 1.2.2
  * ©2016 SpryMedia Ltd - datatables.net/license
  */
 
@@ -1118,7 +1118,7 @@ Buttons.defaults = {
  * @type {string}
  * @static
  */
-Buttons.version = '1.2.2-dev';
+Buttons.version = '1.2.2';
 
 
 $.extend( _dtButtons, {
