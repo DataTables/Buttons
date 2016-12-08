DataTables/Buttons

commit 767a24918b9ea71d2af3d3620fe47d776a28de5e

Performance: Reuse the cell selector when exporting data, so cells need only be selected once (rather than twice as before) Performance: Faster export of data by allowing `-api cells()` to select the rows, rather than getting an array of row indexes first. The former approach enters the row selector into a slow array lookup rather than allowing it to just build the array.
Thread 36630. Thanks Wuigi!

js/dataTables.buttons.js
@@ -1588,13 +1588,11 @@ var _exportData = function ( dt, inOpts )
 		} ).toArray() :
 		null;
 
-	var rowIndexes = dt.rows( config.rows, config.modifier ).indexes().toArray();
-	var cells = dt
-		.cells( rowIndexes, config.columns )
+	var selectedCells = dt.cells( config.rows, config.columns, config.modifier );
+	var cells = selectedCells
 		.render( config.orthogonal )
 		.toArray();
-	var cellNodes = dt
-		.cells( rowIndexes, config.columns )
+	var cellNodes = selectedCells
 		.nodes()
 		.toArray();
 

commit 0706498d00cf03519fe3d37bd6c676cb7c8d6e11

Fix: Exported data would not correctly take into accoun the `selected: true` modifier.
- Caused by 767a24918b9ea71d2af3d3620fe47d776a28de5e
- Mostly reverted, but only using `cells()` once now
- Fixes #108

js/dataTables.buttons.js
@@ -1589,7 +1589,8 @@ var _exportData = function ( dt, inOpts )
 		} ).toArray() :
 		null;
 
-	var selectedCells = dt.cells( config.rows, config.columns, config.modifier );
+	var rowIndexes = dt.rows( config.rows, config.modifier ).indexes().toArray();
+	var selectedCells = dt.cells( rowIndexes, config.columns );
 	var cells = selectedCells
 		.render( config.orthogonal )
 		.toArray();