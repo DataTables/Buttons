6338bdb331f51436305943f0e35f4a69eddd7237
Nick Maev <trixon@mail.ru>
2017-01-24 18:33:58 +0300

    Fix: prevent converting string with leading zero to number.

--- js/buttons.html5.js
+++ js/buttons.html5.js
@@ -1046,32 +1046,36 @@ DataTable.ext.buttons.excelHtml5 = {
 				}
 
 				row[i] = $.trim( row[i] );
+				
+				if (row[i].slice(0, 1) !== "0") {
+					// Prevent converting string with leading zero to number
+					
+					// Special number formatting options
+					for ( var j=0, jen=_excelSpecials.length ; j<jen ; j++ ) {
+						var special = _excelSpecials[j];
 
-				// Special number formatting options
-				for ( var j=0, jen=_excelSpecials.length ; j<jen ; j++ ) {
-					var special = _excelSpecials[j];
+						if ( row[i].match && row[i].match( special.match ) ) {
+							var val = row[i].replace(/[^\d\.\-]/g, '');
 
-					if ( row[i].match && row[i].match( special.match ) ) {
-						var val = row[i].replace(/[^\d\.\-]/g, '');
+							if ( special.fmt ) {
+								val = special.fmt( val );
+							}
 
-						if ( special.fmt ) {
-							val = special.fmt( val );
+							cell = _createNode( rels, 'c', {
+								attr: {
+									r: cellId,
+									s: special.style
+								},
+								children: [
+									_createNode( rels, 'v', { text: val } )
+								]
+							} );
+
+							break;
 						}
-
-						cell = _createNode( rels, 'c', {
-							attr: {
-								r: cellId,
-								s: special.style
-							},
-							children: [
-								_createNode( rels, 'v', { text: val } )
-							]
-						} );
-
-						break;
 					}
 				}
-
+				
 				if ( ! cell ) {
 					if ( typeof row[i] === 'number' || (
 						row[i].match &&
