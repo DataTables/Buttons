2497598ba5e63a5cc972449f2d4539cfe9ec4e9b
Worthy7 <worthy.vii@gmail.com>
2017-03-02 16:41:05 +0900

    Make excel work with IE8
    Can't believe I managed to fix this...

--- js/buttons.flash.js
+++ js/buttons.flash.js
@@ -787,11 +787,19 @@ function _excelColWidth( data, col ) {
 	return max > 6 ? max : 6;
 }
 
-try {
-	var _serialiser = new XMLSerializer();
-	var _ieExcel;
-}
-catch (t) {}
+  try {
+        if (typeof window.XMLSerializer ==='undefined')
+        {
+            function XMLSerializer(){};
+            XMLSerializer.prototype.serializeToString = function(input){
+                return input.xml
+            };
+        }
+      
+        var  _serialiser = new XMLSerializer();
+        var _ieExcel;
+    }
+    catch (t) {}
 
 /**
  * Convert XML documents in an object to strings
