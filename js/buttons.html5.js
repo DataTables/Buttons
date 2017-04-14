86aa7c2293afc94f13854015c8b377eba6b52645
Allan Jardine <allan.jardine@sprymedia.co.uk>
2017-04-14 16:21:56 +0100

    Fix: Excel output where a string was numeric with a leading 0 would result in the 0 being stripped as it was stored as a number. It is now stored as a string.
    - Fixes #113

--- js/buttons.html5.js
+++ js/buttons.html5.js
@@ -1051,7 +1051,10 @@ DataTable.ext.buttons.excelHtml5 = {
 				for ( var j=0, jen=_excelSpecials.length ; j<jen ; j++ ) {
 					var special = _excelSpecials[j];
 
-					if ( row[i].match && row[i].match( special.match ) ) {
+					// TODO Need to provide the ability for the specials to say
+					// if they are returning a string, since at the moment it is
+					// assumed to be a number
+					if ( row[i].match && ! row[i].match(/^0\d+/) && row[i].match( special.match ) ) {
 						var val = row[i].replace(/[^\d\.\-]/g, '');
 
 						if ( special.fmt ) {
