65b6b7b1345c71b1be53aa78713077314dafe24c
Allan Jardine <allan.jardine@sprymedia.co.uk>
2017-09-19 11:03:30 +0100

    Dev version bump

--- js/dataTables.buttons.js
+++ js/dataTables.buttons.js
@@ -1,4 +1,4 @@
-/*! Buttons for DataTables 1.4.2
+/*! Buttons for DataTables 1.4.3-dev
  * ©2016-2017 SpryMedia Ltd - datatables.net/license
  */
 
@@ -1143,7 +1143,7 @@ Buttons.defaults = {
  * @type {string}
  * @static
  */
-Buttons.version = '1.4.2';
+Buttons.version = '1.4.3-dev';
 
 
 $.extend( _dtButtons, {
