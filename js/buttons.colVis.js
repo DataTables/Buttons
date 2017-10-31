b92e02edc914cfb06029a2519f855c840e773860
Allan Jardine <allan@stargazer>
2017-10-31 09:28:40 +0000

    Fix: ColVis buttons for column headers which contain a `-tag select` would show the `-tag option` text
    Thread 45545. Thanks lazzy

--- js/buttons.colVis.js
+++ js/buttons.colVis.js
@@ -149,7 +149,8 @@ $.extend( DataTable.ext.buttons, {
 			var title = dt.settings()[0].aoColumns[ idx ].sTitle
 				.replace(/\n/g," ")        // remove new lines
 				.replace(/<br\s*\/?>/gi, " ")  // replace line breaks with spaces
-				.replace( /<.*?>/g, "" )   // strip HTML
+				.replace(/<select(.*?)<\/select>/g, "") // remove select tags, including options text
+				.replace(/<.*?>/g, "")   // strip HTML
 				.replace(/^\s+|\s+$/g,""); // trim
 
 			return conf.columnText ?
