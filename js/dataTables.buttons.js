bfd530b2514cf793ed4a8eb9abd0d21ae1a2e0df
Allan Jardine <allan@datatables.net>
2019-10-01 10:18:22 +0000

    Buttons 1.6.0 release

--- js/dataTables.buttons.js
+++ js/dataTables.buttons.js
@@ -1,4 +1,4 @@
-/*! Buttons for DataTables 1.6.0-dev
+/*! Buttons for DataTables 1.6.0
  * ©2016-2019 SpryMedia Ltd - datatables.net/license
  */
 
@@ -1370,7 +1370,7 @@ Buttons.defaults = {
  * @type {string}
  * @static
  */
-Buttons.version = '1.6.0-dev';
+Buttons.version = '1.6.0';
 
 
 $.extend( _dtButtons, {
