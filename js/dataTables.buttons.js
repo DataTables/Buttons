c1ef22213939b0366dc5877463f2f478b0dd9f65
Allan Jardine <allan@datatables.net>
2021-12-03 13:27:00 +0000

    Release 2.1.0 (No change from 2.0.2 - which should have been 2.1.0)

--- js/dataTables.buttons.js
+++ js/dataTables.buttons.js
@@ -1,4 +1,4 @@
-/*! Buttons for DataTables 2.0.2
+/*! Buttons for DataTables 2.1.0
  * ©2016-2021 SpryMedia Ltd - datatables.net/license
  */
 
@@ -1843,7 +1843,7 @@ Buttons.defaults = {
  * @type {string}
  * @static
  */
-Buttons.version = '2.0.2';
+Buttons.version = '2.1.0';
 
 
 $.extend( _dtButtons, {
