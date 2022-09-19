d7d856b0acc40a5c8381d5a4c76a79c3488b7cb2
Allan Jardine <allan@datatables.net>
2022-09-19 12:24:59 +0000

    Dev: Remove UMD from styling files to allow for new build process which adds them

--- js/buttons.bulma.js
+++ js/buttons.bulma.js
@@ -2,39 +2,6 @@
  * ©2021 SpryMedia Ltd - datatables.net/license
  */
 
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net-bm', 'datatables.net-buttons'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		module.exports = function (root, $) {
-			if ( ! root ) {
-				root = window;
-			}
-
-			if ( ! $ || ! $.fn.dataTable ) {
-				$ = require('datatables.net-bm')(root, $).$;
-			}
-
-			if ( ! $.fn.dataTable.Buttons ) {
-				require('datatables.net-buttons')(root, $);
-			}
-
-			return factory( $, root, root.document );
-		};
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document, undefined ) {
-'use strict';
-var DataTable = $.fn.dataTable;
-
 $.extend( true, DataTable.Buttons.defaults, {
 	dom: {
 		container: {
@@ -93,6 +60,3 @@ $.extend( true, DataTable.Buttons.defaults, {
 		return button;
 	}
 } );
-
-return DataTable.Buttons;
-}));
--- js/buttons.semanticui.js
+++ js/buttons.semanticui.js
@@ -2,40 +2,6 @@
  * ©2016 SpryMedia Ltd - datatables.net/license
  */
 
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net-se', 'datatables.net-buttons'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		module.exports = function (root, $) {
-			if ( ! root ) {
-				root = window;
-			}
-
-			if ( ! $ || ! $.fn.dataTable ) {
-				$ = require('datatables.net-se')(root, $).$;
-			}
-
-			if ( ! $.fn.dataTable.Buttons ) {
-				require('datatables.net-buttons')(root, $);
-			}
-
-			return factory( $, root, root.document );
-		};
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document, undefined ) {
-'use strict';
-var DataTable = $.fn.dataTable;
-
-
 $.extend( true, DataTable.Buttons.defaults, {
 	dom: {
 		container: {
@@ -81,7 +47,3 @@ $(document).on('buttons-popover.dt', function () {
 		$('.dtsp-panesContainer').removeClass('vertical buttons')
 	}
 });
-
-
-return DataTable.Buttons;
-}));
