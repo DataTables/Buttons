604499c76d7d7ab5051f13d4308ce667d0901f20
Allan Jardine <allan@datatables.net>
2024-04-10 13:43:33 +0000

    Dev: Tweak comment removal regex to allow for `--!>` closing comments

--- js/dataTables.buttons.js
+++ js/dataTables.buttons.js
@@ -1882,7 +1882,7 @@ Buttons.stripHtmlComments = function (input) {
 	
 	do {  
 	  previous = input;  
-	  input = input.replace(/(<!--.*?-->)|(<!--[\S\s]+?-->)|(<!--[\S\s]*?$)/g, '');  
+	  input = input.replace(/(<!--.*?--!?>)|(<!--[\S\s]+?--!?>)|(<!--[\S\s]*?$)/g, '');  
 	} while (input !== previous);  
 
 	return input;  
