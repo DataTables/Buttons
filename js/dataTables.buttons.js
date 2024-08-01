c863eed3dcf6200ffa23f4917d4eda0c35fc03e4
Allan Jardine <allan@datatables.net>
2024-08-01 13:27:57 +0000

    Release 3.1.1

--- js/dataTables.buttons.js
+++ js/dataTables.buttons.js
@@ -1,4 +1,4 @@
-/*! Buttons for DataTables 3.1.0
+/*! Buttons for DataTables 3.1.1
  * © SpryMedia Ltd - datatables.net/license
  */
 
@@ -1988,7 +1988,7 @@ Buttons.defaults = {
  * @type {string}
  * @static
  */
-Buttons.version = '3.1.0';
+Buttons.version = '3.1.1';
 
 $.extend(_dtButtons, {
 	collection: {
