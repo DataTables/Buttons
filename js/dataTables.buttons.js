8ab9e3841286f90654d29150a6f28c7b286f5eb1
Allan Jardine <allan@datatables.net>
2024-10-11 09:55:18 +0000

    Docs: Popover description was missing information about `fixed` positioning
    Dev: Remove debug

--- js/dataTables.buttons.js
+++ js/dataTables.buttons.js
@@ -1338,8 +1338,6 @@ $.extend(Buttons.prototype, {
 			inOpts
 		);
 
-		console.log(inOpts, options);
-
 		var containerSelector =
 			options.tag + '.' + options.containerClassName.replace(/ /g, '.');
 		var hostNode = hostButton.node();
