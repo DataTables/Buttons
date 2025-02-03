32d8086656b98aba872266b85c8eb4ac371c04e8
Allan Jardine <allan@datatables.net>
2025-02-03 11:26:10 +0000

    Fix: PDF export footer wasn't being styled as a footer, but as a header.
    https://datatables.net/forums/discussion/80540

--- js/buttons.html5.js
+++ js/buttons.html5.js
@@ -1496,7 +1496,7 @@ DataTable.ext.buttons.pdfHtml5 = {
 									text: cell.title,
 									colSpan: cell.colspan,
 									rowSpan: cell.rowspan,
-									style: 'tableHeader'
+									style: 'tableFooter'
 							}
 							: {};
 					})
@@ -1556,7 +1556,8 @@ DataTable.ext.buttons.pdfHtml5 = {
 				},
 				tableFooter: {
 					bold: true,
-					fontSize: 11
+					fontSize: 11,
+					alignment: 'center'
 				},
 				table: {
 					margin: [0, 5, 0, 5]
